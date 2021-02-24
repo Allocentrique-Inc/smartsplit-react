e02e8c67a89e61c6725d665f3bd053d68716817c
Simon Boisclair <simonboisclair553@hotmail.com>
2021-02-24 13:45:23 -0500

    apiAdress

--- src/api/workpieces/postWorkpiece.js
+++ src/api/workpieces/postWorkpiece.js
@@ -2,7 +2,7 @@ const postWorkpiece = async (payload) => {
   const { title, type, file } = payload;
   const body = JSON.stringify({ title });
   try {
-    const url = 'http://localhost:3001/v1/workpieces/';
+    const url = 'http://159.203.15.16:3001/v1/workpieces/';
     const method = 'POST';
     const bearer = `Bearer ${localStorage.getItem('accessToken')}`;
     const response = await fetch(url, {
