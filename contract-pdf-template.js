b0f25a828ce06cff6d4508397d98d71e8f21e104
François-Xavier Dueymes <fx.dueymes@protonmail.com>
2021-03-04 19:11:07 -0500

    Add contract pdf template

---
+++ contract-pdf-template.js
@@ -0,0 +1,47 @@
+export default {
+  header:
+    '<h2>ENTENTE DE PARTAGE DE DROITS</h2><p><i>Droit d’auteur sur l’oeuvre & Droits voisins des artistes-interprètes et producteurs</i></p>',
+  sections: {
+    generalInformations: {
+      title: 'Entente concernant la pièce musicale originale :',
+      content:
+        '<strong>Monsieur Poirier (feat. Claude Poirier)</strong> interprétée par <i>Luminol</i>, ci-après la «<b>pièce</b>» musicale.',
+    },
+    rightHolders: {
+      title: 'Entente intervenue entre les contributeurs suivants :',
+      list: [
+        "<h1>1</h1><p><b>Claude Poirier</b> portant le nom de ... <span class='ipi'>- IPI #518507355</span></p><p>Domicilié au 54 Place Calais, Candiac, Québec, Canada, J5R 4K6 <br>Et joignable par téléphone au (514) 594-2424 et par courriel au claudep38@icloud.com.<a href='mailto:claudep38@icloud.com'>claudep38@icloud.com</a></p>",
+        '...',
+        '<i>Tous ci-dessus collectivement nommés les «contributeurs» ou les «parties»</i>',
+      ],
+    },
+    rightSplits: {
+      title:
+        'Les contributeurs s’entendent sur ces partages de droits en lien avec la «pièce» :',
+       copyright: [
+          {
+            avatar: '?',
+            name: 'Frédéric Desjardins (Luminol)',
+            roles: 'Auteur, Arrangeur, Compositeur',
+            share: 75,
+          },
+          '...',
+        ],
+        performance: [],
+        recording: [],
+    },
+    agreementConditions: {
+      title: 'Conditions de l’entente',
+      description: '<p>La présente entente est en vigueur pour la durée des droits de chacun des contributeurs relativement à la pièce musicale.</p><p>Par la présente, les contributeurs acceptent les différents partages de droits présentés à la page précédente, soit le partage relatif au droit d’auteur sur l’oeuvre musicale, au droit voisin lié aux prestations ainsi que le droit voisin lié à l’enregistrement sonore de la pièce musicale <strong>Monsieur Poirier (feat. Claude Poirier)</strong>.</p>',
+      copyright: {
+        title: "DROITS D'AUTEUR",
+        content: "<ol><li>Chaque contributeur ayant participé à la <i>création</i> de l’oeuvre <strong>Monsieur Poirier (feat. Claude Poirier)</strong> :<ol><li>représente et garantit que l’oeuvre est originale et qu’elle n’enfreint pas les droits de tiers, incluant tout droit de propriété intellectuelle;</li><li>comprend qu’il peut, à sa discrétion, céder, concéder une licence sur ou faire administrer sa portion respective des droits d’auteur sur l’oeuvre à des tiers de son choix (p. ex. éditeurs, administrateur d’éditions, co-éditeurs, sous-éditeurs);</li><li>représente et garantit qu'il a le droit et la capacité de conclure et respecter la présente entente et qu’il n'a aucune restriction contractuelle ou autre l’empêchant de conclure et respecter la présente entente.</li></ol></li></ol>"
+      },
+      performance: {
+        title: 'INTERPRÉTATION',
+        content: "<ol></ol>"
+
+      }
+  },
+  footer: 'Entente de partage de droits - Page...',
+}
