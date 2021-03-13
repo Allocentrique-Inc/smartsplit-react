3e435fb67930a681b4f2ff0556683b990ca4ef1d
will <will.eizlini@gmail.com>
2021-03-13 07:42:54 -0500

    avatarUrl is relative; add //host:port/v1 prefix

--- src/components/dashboard/_/avatar/avatar.js
+++ src/components/dashboard/_/avatar/avatar.js
@@ -5,10 +5,10 @@ const Avatar = (props) => {
   const { className } = props;
   const { user } = props;
   const initials = user ? user.firstName.substr(0, 1) + user.lastName.substr(0, 1) : '';
-  const hasImage = user.avatar || user.avatarUrl;
+  const hasImage = user.avatarUrl;
   return (
     <div className={`avatar ${className}`} title={user.fullName}>
-      {hasImage && <img src={user.avatar ? user.avatar : user.avatarUrl} alt={user.fullName} />}
+      {hasImage && <img src={Config.apiUrl + user.avatarUrl} alt={user.fullName} />}
       {!hasImage && <div className="avatar-placeholder"><span>{initials}</span></div>}
     </div>
   );
