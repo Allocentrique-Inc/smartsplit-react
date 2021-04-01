21437d00ab77dab67274d0f01a6a581587b56848
will <will.eizlini@gmail.com>
2021-04-01 08:54:40 -0400

    css fixes for orientation page

--- src/components/dashboard/_/avatar/avatar.js
+++ src/components/dashboard/_/avatar/avatar.js
@@ -14,8 +14,6 @@ const Avatar = (props) => {
         <img
           src={user.avatar ? user.avatar : user.avatarUrl}
           alt={user.artistName ? user.artistName : user.fullName}
-          style={{ webkitFilter: color === '#ccc' ? 'grayscale(100%)' : 'none',
-            filter: color === '#ccc' ? 'grayscale(100%)' : 'none' }}
         />
       )}
       {!hasImage && (
